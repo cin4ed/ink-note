7abc652b75b0ef8a7ed8c1000f6deb09e95fc0ff
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-09-21 16:37:00 -0700

    surrendered to doing

--- app/components/note.tsx
+++ app/components/note.tsx
@@ -1,3 +1,45 @@
-"use client";
+import { useState } from "react";
 
-export function Note() {}
+import { Rnd } from "react-rnd";
+import { DragIcon } from "./drag-icon";
+
+export function Note() {
+  const [showDraggableHandle, setShowDraggableHandle] = useState(false);
+
+  return (
+    <div>
+      <Rnd
+        className="border overflow-hidden flex flex-col rounded"
+        default={{
+          x: 0,
+          y: 0,
+          width: 200,
+          height: 200,
+        }}
+        dragHandleClassName="drag-handle"
+      >
+        <div
+          className="bg-black text-white flex items-center"
+          onMouseOver={() => setShowDraggableHandle(true)}
+          onMouseOut={() => setShowDraggableHandle(false)}
+        >
+          {showDraggableHandle && (
+            <div className="cursor-move w-5 flex justify-center drag-handle">
+              <DragIcon width={50} height={20} fill="white" />
+            </div>
+          )}
+          <input
+            className="flex-1 bg-inherit outline-none px-1"
+            type="text"
+            placeholder="Untitled"
+          />
+          <button className="w-10">X</button>
+        </div>
+        <textarea
+          className="flex-1 outline-none resize-none p-1"
+          placeholder="Write Something..."
+        ></textarea>
+      </Rnd>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
 "use client";
 
-import { ResizableDiv } from "./components/resizable-div";
+import { Note } from "./components/note";
 
 export default function Home() {
-  return (
-    <div>
-      <ResizableDiv>
-        <h1>Lorem Ipsum</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
-          libero nihil, velit sequi, dolorum blanditiis unde ab adipisci a
-          expedita necessitatibus neque quis veritatis iure repellendus possimus
-          nobis rem culpa dolores minus eius numquam. Pariatur mollitia
-          assumenda expedita deserunt tenetur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          cupiditate quis ullam, molestias velit cum ad unde doloremque fugiat
-          nulla assumenda iusto ipsam tenetur nostrum necessitatibus quas
-          reiciendis laborum. Inventore doloremque quas ut, magnam ipsam
-          sapiente voluptates aliquid soluta aliquam.
-        </p>
-      </ResizableDiv>
-    </div>
-  );
+  return <Note />;
 }
